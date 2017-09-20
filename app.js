8a3953f245f0dc201907478b9698eef17f87958b
Rahul Goti <gotirahul1@gmail.com>
2017-09-20 21:16:27 +0530

    Update app.js
    Synch

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ todoController(app);
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+console.log('You are listing to port ' + port);
+console.log('You are listing to ip   ' + ip );
 //listen to port
 app.listen(port, ip);
 console.log('You are listing to port 3000');
